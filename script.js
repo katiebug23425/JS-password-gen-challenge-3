cdacfd02b6e320d061e14b571cd9d3cd78544bdc
Katie Bug <katierosealford@gmail.com>
2024-02-22 14:19:15 -0500

    added if statements and the finished the JS code to generate and display the password.

--- script.js
+++ script.js
@@ -2,10 +2,11 @@
 var generateBtn = document.querySelector("#generate");
 
 //Criteria selection for password//
-let length = prompt('How many characters would you like your password to be?');
+function generatePassword() {let length = prompt('How many characters would you like your password to be?');
 if (length < 8 || length > 128) {
-  alert("Your password must be between 8 and 128 characters");
-};
+  alert("Your password must be between 8 and 128 characters"); 
+return;
+}; 
 let numbers = confirm('Would you like to include numbers in your password?');
 let specials = confirm('Would you like to include symbols on your password?');
 let uppercase = confirm('Would you like to include uppercase letters in your password?');
@@ -13,13 +14,40 @@ let lowercase = confirm('Would you like to include lowercase letters in your pas
 
 if (numbers === false && specials === false && uppercase === false & lowercase === false) {
   alert('Your password does not meet the required criteria, you must select at least one option')
-}
-
+}; 
 //options for password criteria//
+var master = ''
 var numbers2 = '0123456789'
-var SpecialSymbols = '!@#$%^&*()+=?<\>-'
+var specialSymbols = '!@#$%^&*()+=?<\>-'
 var uppercaseAlphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 var lowercaseAlphabet = 'abcdefghijklmnopqrstuvwxyz';
+
+if (numbers === true) { master += numbers2 
+
+
+};
+if (specials === true) { master += specialSymbols
+
+};
+
+if (uppercase === true) { master += uppercaseAlphabet
+};
+
+if (lowercase === true) { master += lowercaseAlphabet
+
+
+};
+
+var password2 = ''
+for (i=0; i < length; i++) {
+ var Random = Math.floor(Math.random() * master.length)
+password2 += master.charAt(Random)
+}
+return password2;
+
+}
+
+
 
 // Write password to the #password input
 function writePassword() {
